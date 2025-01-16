845ab29ed1039ff87f198d65f46a3e9c20c05edf
Dmitrii <d.n.baklai@gmail.com>
2025-01-16 09:29:15 +0200

    Упорядочить импорты в роли.guard.ts для улучшения читаемости кода

--- src/common/guards/roles.guard.ts
+++ src/common/guards/roles.guard.ts
@@ -1,7 +1,6 @@
-import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { Observable } from 'rxjs';
-
 import { Roles } from '../decorators/roles.decorator';
 
 @Injectable()
